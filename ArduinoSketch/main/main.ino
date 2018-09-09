833f20b1a974d6fe7aff690306d88b7e58777155
Vinicius de Santana <vinicius.vts@gmail.com>
2018-09-09 16:37:36 -0300

    criado o arquivo que será colocado no controlador

---
+++ ArduinoSketch/main/main.ino
@@ -0,0 +1,14 @@
+/*
+  BuildingAutomation
+  Aplicação com o proposito de coordenar ativação e desativação
+  automática de eletro-eletronicos em predios comerciais
+*/
+
+String serialReturn;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+}
