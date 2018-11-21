a69cfb76e90468ce68ab06fa292539a0e1bc7f75
Jacksonnascimento <44805133+Jacksonnascimento@users.noreply.github.com>
2018-11-21 10:08:05 -0300

    Update BD_automaçãoPredial.sql

--- BD_automaçãoPredial.sql
+++ BD_automaçãoPredial.sql
@@ -5,3 +5,11 @@ create table sala ( id char (18) not null,
  numeroSala char (25), horaLigar datetime not null,
  horaDesligar datetime not null, primary key (id));
  
+INSERT INTO sala values ('18A101', 'A1', '2018-11-27 21:35:00',
+ '2018-11-27 18:35:00');
+ 
+INSERT INTO sala values ('18B606', 'B606', '2018-11-27 14:12:00',
+ '2018-11-27 20:35:00');
+ 
+ INSERT INTO sala values ('18B201', 'A201', '2018-11-27 19:15:00',
+ '2018-11-27 21:35:00');
